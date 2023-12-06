3d9889af95fe7bda0015a05eddcbe25083e33faa
AumJavalgikar <99173843+AumJavalgikar@users.noreply.github.com>
2023-12-07 01:02:50 +0530

    Beginning of the agent system, still need to interface with Godot (#2)

---
+++ python/API_endpoint.py
@@ -0,0 +1,123 @@
+from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+import openai
+from openai import AsyncOpenAI
+import yaml
+import json
+
+load_dotenv('secrets/secrets.env')
+
+app = FastAPI()
+
+client = AsyncOpenAI(
+    api_key=os.getenv('OPENAI_KEY'),
+)
+
+@app.get("/")
+async def test():
+    response = await client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": 'This is a test, respond with "GPT works"'},
+            {"role": "user", "content": ''},
+            {"role": "assistant", "content": ''}
+        ]
+    )
+    conversation_response = response.choices[0].message.content
+    return conversation_response
+
+
+@app.get("/getConversationResponse/")
+async def read_item(conversation_info: dict):
+    '''
+    conversation_info format :
+    {
+    'current_speaker' : 'character1_name',
+    'other_speaker': 'character2_name',
+    'current_time' : 'time',
+    'new_response' : 'new_response' # Needed for user sent messages
+    conversation_initiated : True or False
+    conversation_initiate_reason : 'reason to start conversation'
+    }
+    '''
+
+    current_speaker = conversation_info.get('current_speaker')
+    other_speaker = conversation_info.get('other_speaker')
+    current_time = conversation_info.get('current_time')
+    new_response = conversation_info.get('new_response')
+    conversation_initiated = conversation_info.get('conversation_initiated')
+    conversation_history = get_conversation_history(current_speaker, other_speaker)
+
+    if conversation_initiated:
+        conversation_initate_reason = conversation_info.get('conversation_initate_reason')
+        last_response = f'''
+        You decided to initiate conversation with {other_speaker} for the following reason : {conversation_initate_reason}\n
+        Now you can say something to begin the conversation
+        '''
+    else:
+        last_response = f'''
+        Last response by {other_speaker} :
+        {current_time} {other_speaker} : {new_response}
+        '''
+
+    with open('prompts/conversation/base_prompt.yaml') as f:
+        base_prompt = yaml.safe_load(f)
+        system_prompt = base_prompt.get('system')
+        user_prompt = base_prompt.get('user_prompt')
+        system_prompt.format(current_speaker_details=get_character_details(current_speaker),
+                             other_speaker=other_speaker,
+                             other_speaker_details=get_character_details(other_speaker),
+                             conversation_history=conversation_history)
+        user_prompt.format(last_response=last_response)
+
+    response = await client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+            {"role": "assistant", "content": ""}
+            ]
+    )
+
+    conversation_response = json.loads(response.choices[0].message.content, strict=False)
+
+    update_conversation_history(current_speaker, other_speaker, current_time, conversation_response)
+
+    return conversation_response
+
+
+def get_conversation_history(char1, char2):
+
+    # Check if conversation exists, if not, create a new conversation
+    try:
+        with open(f'conversations/{char1}/{char2}', 'r') as f:
+            pass
+    except FileNotFoundError:
+        with open(f'conversations/{char1}/{char2}', 'w') as f:
+            f.write(f'Conversation history for {char1} and {char2}\n')
+
+        with open(f'conversations/{char1}/{char2}', 'w') as f:
+            f.write(f'Conversation history for {char1} and {char2}\n')
+        with open(f'conversations/{char2}/{char1}', 'w') as f:
+            f.write(f'Conversation history for {char2} and {char1}\n')
+
+    with open(f'conversations/{char1}/{char2}', 'r') as f:
+        history = f.read()
+
+    return history
+
+
+def update_conversation_history(char1, char2, timestamp, new_response):
+    with open(f'conversations/{char1}/{char2}', 'a') as f:
+        f.write(f'\n{timestamp} {char1} : {new_response}')
+
+    with open(f'conversations/{char1}/{char2}', 'a') as f:
+        f.write(f'\n{timestamp} {char1} : {new_response}')
+
+
+def get_character_details(character):
+    with open(f'prompts/character_details/{character}.yaml', 'r') as f:
+        details = yaml.safe_load(f)
+    return details
+
