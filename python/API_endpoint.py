eb7499593986400de9d23ec84d1c41ece09bac4a
AumJavalgikar <99173843+AumJavalgikar@users.noreply.github.com>
2023-12-12 15:15:56 +0530

    Agent system (#3)
    * Beginning of the agent system, still need to interface with Godot
    
    * working on Agent system
    
    * working on Agent system
    
    * Tuned Agent management system

--- python/API_endpoint.py
+++ python/API_endpoint.py
@@ -1,3 +1,6 @@
+import random
+import uuid
+
 from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
@@ -5,6 +8,7 @@ import openai
 from openai import AsyncOpenAI
 import yaml
 import json
+import re
 
 load_dotenv('secrets/secrets.env')
 
@@ -12,12 +16,14 @@ app = FastAPI()
 
 client = AsyncOpenAI(
     api_key=os.getenv('OPENAI_KEY'),
+    base_url="https://openrouter.ai/api/v1"
 )
+
 
 @app.get("/")
 async def test():
     response = await client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="huggingfaceh4/zephyr-7b-beta",
         messages=[
             {"role": "system", "content": 'This is a test, respond with "GPT works"'},
             {"role": "user", "content": ''},
@@ -29,7 +35,7 @@ async def test():
 
 
 @app.get("/getConversationResponse/")
-async def read_item(conversation_info: dict):
+async def getConversationResponse(conversation_info: dict):
     '''
     conversation_info format :
     {
@@ -53,42 +59,117 @@ async def read_item(conversation_info: dict):
         conversation_initate_reason = conversation_info.get('conversation_initate_reason')
         last_response = f'''
         You decided to initiate conversation with {other_speaker} for the following reason : {conversation_initate_reason}\n
-        Now you can say something to begin the conversation
+        Say something using the given syntax.
         '''
     else:
         last_response = f'''
-        Last response by {other_speaker} :
-        {current_time} {other_speaker} : {new_response}
+        {other_speaker} said '{new_response}'
+        Respond to this using the given syntax.
         '''
 
     with open('prompts/conversation/base_prompt.yaml') as f:
         base_prompt = yaml.safe_load(f)
         system_prompt = base_prompt.get('system')
-        user_prompt = base_prompt.get('user_prompt')
-        system_prompt.format(current_speaker_details=get_character_details(current_speaker),
-                             other_speaker=other_speaker,
-                             other_speaker_details=get_character_details(other_speaker),
-                             conversation_history=conversation_history)
-        user_prompt.format(last_response=last_response)
+        user_prompt = base_prompt.get('user')
+        system_prompt = system_prompt.format(current_speaker_details=get_character_details(current_speaker),
+                                             other_speaker=other_speaker,
+                                             other_speaker_details=get_character_details(other_speaker),
+                                             conversation_history=conversation_history)
+        user_prompt = user_prompt.format(last_response=last_response,
+                                         current_speaker=current_speaker,
+                                         )
+
+    print(system_prompt)
+    print(user_prompt)
 
     response = await client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="huggingfaceh4/zephyr-7b-beta",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt},
             {"role": "assistant", "content": ""}
-            ]
+        ]
     )
+    print(response.choices[0].message.content)
+    regex = re.compile('({.*})')
+    json_response = regex.findall(response.choices[0].message.content)[0]
+    conversation_response = json.loads(json_response, strict=True)
 
-    conversation_response = json.loads(response.choices[0].message.content, strict=False)
-
-    update_conversation_history(current_speaker, other_speaker, current_time, conversation_response)
+    update_conversation_history(current_speaker, other_speaker, current_time, conversation_response, new_response)
 
     return conversation_response
 
 
-def get_conversation_history(char1, char2):
+@app.get("/getAction/")
+async def getAction(action_info: dict):
+    '''
+    conversation_info format :
+    {
+    'character_name' : 'character1_name',
+    'current_time': 'time',
+    'other_character_status' : 'statuses'
+    }
+    '''
 
+    character_name = action_info.get('character_name')
+    current_time = action_info.get('current_time')
+    other_character_status = action_info.get('other_character_status')
+    journal = get_journal(character_name)
+
+    with open('prompts/actions/base_prompt.yaml') as f:
+        base_prompt = yaml.safe_load(f)
+        system_prompt = base_prompt.get('system')
+        user_prompt = base_prompt.get('user')
+        system_prompt = system_prompt.format(current_time=current_time,
+                                             other_character_status=other_character_status,
+                                             current_speaker_details=get_character_details(character_name),
+                                             journal=journal)
+        user_prompt = user_prompt.format(character_name=character_name,
+                                             random_seed=random.randint(0, 10000))
+
+    print(system_prompt)
+    print(user_prompt)
+
+    response = await client.chat.completions.create(
+        model="huggingfaceh4/zephyr-7b-beta",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+            {"role": "assistant", "content": ""}
+        ]
+    )
+    print(response.choices[0].message.content)
+    regex = re.compile('({.*})')
+    json_response = regex.findall(response.choices[0].message.content)[0]
+    conversation_response = json.loads(json_response, strict=True)
+
+    update_journal(character_name, conversation_response, time_stamp=current_time)
+
+    return conversation_response
+
+
+def get_journal(character_name):
+    try:
+        with open(f'journals/{character_name}', 'r') as f:
+            pass
+    except FileNotFoundError:
+        with open(f'journals/{character_name}', 'w') as f:
+            f.write(f'Journal for {character_name}\n')
+
+    with open(f'journals/{character_name}', 'r') as f:
+        journal = f.read()
+
+    return journal
+
+
+def update_journal(character_name, action, time_stamp):
+    new_entry = action['journal_entry']
+
+    with open(f'journals/{character_name}', 'a') as f:
+        f.write(f'\n{time_stamp} : {new_entry}')
+
+
+def get_conversation_history(char1, char2):
     # Check if conversation exists, if not, create a new conversation
     try:
         with open(f'conversations/{char1}/{char2}', 'r') as f:
@@ -108,16 +189,18 @@ def get_conversation_history(char1, char2):
     return history
 
 
-def update_conversation_history(char1, char2, timestamp, new_response):
+def update_conversation_history(char1, char2, timestamp, new_response, old_response):
+    new_response = new_response['conversation_response']
     with open(f'conversations/{char1}/{char2}', 'a') as f:
+        f.write(f'\n{timestamp} {char2} : {old_response}')
         f.write(f'\n{timestamp} {char1} : {new_response}')
 
-    with open(f'conversations/{char1}/{char2}', 'a') as f:
+    with open(f'conversations/{char2}/{char1}', 'a') as f:
+        f.write(f'\n{timestamp} {char2} : {old_response}')
         f.write(f'\n{timestamp} {char1} : {new_response}')
 
 
 def get_character_details(character):
     with open(f'prompts/character_details/{character}.yaml', 'r') as f:
         details = yaml.safe_load(f)
-    return details
-
+    return details['character_details']
